23f08ff604f16726235509d85b73bdacc1fc8cf8
Gary Tapia <gary.tapia2009@gmail.com>
2021-07-23 16:07:36 -0500

    commenting out invitee portion temporarily

--- securityhub.tf
+++ securityhub.tf
@@ -1,9 +1,19 @@
 resource "aws_securityhub_account" "SecurityHub" {}
 
 
-resource "aws_securityhub_member" "SecurityHub_Addon_Member" {
-  depends_on = [aws_securityhub_account.SecurityHub]
-  account_id = var.SECURITY_ACCOUNT_ID
-  email      = var.CONTACT_EMAIL
-  invite     = true
-}
+# resource "aws_securityhub_member" "SecurityHub_Addon_Member" {
+#   depends_on = [aws_securityhub_account.SecurityHub]
+#   #account_id = var.SECURITY_ACCOUNT_ID
+#   email      = var.CONTACT_EMAIL
+#   invite     = true
+# }
+
+# resource "aws_securityhub_account" "invitee" {
+#   provider = "aws.invitee"
+# }
+
+# resource "aws_securityhub_invite_accepter" "invitee" {
+#   provider   = "aws.invitee"
+#   depends_on = [aws_securityhub_account.accepter]
+#   master_id  = aws_securityhub_member.SecurityHub_Addon_Member.master_id
+# }
